8087acdc44e791a02e5577e9158650f0fbd703fb
denisratskov <86842448+denisratskov@users.noreply.github.com>
2021-08-16 23:43:05 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,3 +14,19 @@ module.exports = function toReadable (number) {
   0.9
 }
 toReadable(0.9); //вернет "девятьсот девяносто девять"
+module.exports = function toReadable (number) {
+  999
+}
+toReadable(9999); //вернет "девятьсот девяносто девять"
+module.exports = function toReadable (number) {
+  9
+}
+toReadable(9); //вернет "девятьсот девяносто девять"
+module.exports = function toReadable (number) {
+  99999
+}
+toReadable(99999); //вернет "девятьсот девяносто девять"
+module.exports = function toReadable (number) {
+  0.9
+}
+toReadable(0.9); //вернет "девятьсот девяносто девять"
